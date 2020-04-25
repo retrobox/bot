44e81910067b024e39dfa8675d8f147d308bf25c
Valentin Derouet <thevalentin61@gmail.com>
2020-04-25 20:06:15 +0200

    feat(command): add purge command & update discordjs + some refactoring

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# Discord's bot [WIP]
+# Discord's bot of RetroBox server
+
+## Features
+
+- Remove message hisotry in channel with the command `!purge "number of messages you want to delete"`    
+**Exemple:** ```!purge 20``` will delete 20 message from every users in the channel who where the command was executed. You need to have MANAGE_MESSAGE permission to da that.
+
+We will add others features asap. 
 
 ## Install
 
--- index.js
+++ index.js
@@ -1,23 +1,48 @@
+require("dotenv").config();
+
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-require("dotenv").config();
-
 var opus = require("opusscript");
-var prefix = "!";
+var PREFIX = "!";0
 
 bot.on("ready", function() {
-  bot.user.setActivity("!retrobox", {"type": "PLAYING"});
-  console.log("Je suis connecté !");
+  bot.user.setActivity(PREFIX + "retrobox", {"type": "PLAYING"});
+  console.log("Connected.");
 });
 
-bot.on("message", message => {
-  if (message.content === prefix + "retro") {
-    message.channel.send("TEST", {
+bot.on("message", async message => {
+  if (message.content.charAt(0) !== PREFIX) {
+  	return;
+  }
+	let args = message.content.split(' ')
+  args[0] = args[0].substr(PREFIX.length)
+  if (args[0] == "retrobox") {
+    message.channel.send("", {
       file:
         "https://cdn.discordapp.com/attachments/353261287247183874/423204038604095488/01.jpg"
     });
   }
+
+  if (args[0] == "ping") {
+      message.channel.send("Pong!");
+  }
+
+  if (args[0] == "purge") {
+    // on fait la vérification de permission
+    if (!message.member.permissions.has(Discord.Permissions.FLAGS.MANAGE_MESSAGES, true)) return message.reply('You bastard!')
+    
+    if (args.length == 1) return message.reply('You haven\'t given an amount of messages which should be deleted!'); 
+    if (isNaN(args[1])) return message.reply('The amount parameter isn`t a number!'); 
+
+    if (args[1] > 100) return message.reply('You can`t delete more than **100** messages at once!'); 
+    if (args[1] < 1) return message.reply('You have to delete at least **1** message!'); 
+
+    await message.channel.messages.fetch({ limit: args[1] }).then(messages => {
+      message.channel.bulkDelete(messages)
+      message.reply(':white_check_mark: You deleted: **' + messages.size + '** messages')
+    });
+	}
 });
 
 bot.login(process.env.DISCORD_TOKEN);
